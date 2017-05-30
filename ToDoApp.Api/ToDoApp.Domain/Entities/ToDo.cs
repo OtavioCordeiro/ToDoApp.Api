77c54c8e422277ccbcd8a70800c29d29f19f5602
OtavioCordeiro <otavionew@hotmail.com>
2017-05-30 00:01:28 -0300

    :heavy_plus_sign: implementa camada de domínio

---
+++ ToDoApp.Api/ToDoApp.Domain/Entities/ToDo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToDoApp.Domain.Entities
+{
+   public  class Todo
+    {
+        public int Id { get; set; }
+
+        public string Text { get; set; }
+
+        public bool IsCompleted { get; set; }
+    }
+}
---
+++ ToDoApp.Api/ToDoApp.Domain/Filters/ToDoFilter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToDoApp.Domain.Filters
+{
+    public class ToDoFilter
+    {
+        public Nullable<int> Id { get; set; }
+
+        public string Text { get; set; }
+
+        public Nullable<bool> IsCompleted { get; set; }
+    }
+}
