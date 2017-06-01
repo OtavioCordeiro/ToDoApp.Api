91a0c3019065bfd015c1a74d43ea7759e88d4ea0
OtavioCordeiro <otavionew@hotmail.com>
2017-06-01 16:12:25 -0300

    :heavy_plus_sign: Implementa camada de DomainServices

---
+++ ToDoApp.Api/ToDoApp.DomainServices/IoC/Module.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ToDoApp.DomainServices.Interfaces;
+
+namespace ToDoApp.DomainServices.IoC
+{
+    public static class Module
+    {
+        public static Dictionary<Type, Type> GetTypes()
+        {
+            Dictionary<Type, Type> dictionary = new Dictionary<Type, Type>();
+
+            dictionary.Add(typeof(IToDoDomainService), typeof(ToDoDomainService));
+
+            return dictionary;
+        }
+    }
+}
---
+++ ToDoApp.Api/ToDoApp.DomainServices/ToDoDomainService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ToDoApp.Domain.Entities;
+using ToDoApp.Domain.Filters;
+using ToDoApp.Domain.Repositories;
+using ToDoApp.DomainServices.Interfaces;
+
+namespace ToDoApp.DomainServices
+{
+    internal class ToDoDomainService : IToDoDomainService
+    {
+        private readonly IToDoRepository repository;
+
+        public ToDoDomainService(IToDoRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public Todo Create(Todo todo)
+        {
+            return this.repository.Create(todo);
+        }
+
+        public bool Delete(int id)
+        {
+            return this.repository.Delete(id);
+        }
+
+        public Todo GetById(int id)
+        {
+            return this.repository.GetById(id);
+        }
+
+        public IEnumerable<Todo> List(ToDoFilter filter)
+        {
+            return this.repository.List(filter);
+        }
+
+        public bool Update(Todo todo)
+        {
+            return this.repository.Update(todo);
+        }
+    }
+}
